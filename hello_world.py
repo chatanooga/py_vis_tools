e6cbe90b9fef9ff73b36e10402024c2aecec26b8
chatanooga <chatanooga@gmail.com>
2014-01-10 15:55:47 +0000

    Update hello_world.py
    clean up after merge debacle

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,5 @@
 print "hello world"
 print "goodbye world"
 print "so long world"
-<<<<<<< HEAD
-print " more stuffsdfsdfsdf"
-print "change from temp repo"
-=======
-print " more stuffsdfsdfsdfspaddy"
->>>>>>> a66106724a29a1b7cde447652edd7aac574d028d
+
 
